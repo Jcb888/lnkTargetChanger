3a530310c983cb7f14c285cd7122e3156a229821
Jean-Christophe BILLARD <jchristophe.billard@outlook.com>
2017-07-26 19:30:54 +0200

    debut ajout sauvegarde avant modif lnk

--- lnkTargetChanger/Form1.Designer.cs
+++ lnkTargetChanger/Form1.Designer.cs
@@ -28,12 +28,148 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.comboBoxTxt2Change = new System.Windows.Forms.ComboBox();
+            this.comboBoxNouveauPrefix = new System.Windows.Forms.ComboBox();
+            this.buttonExecuter = new System.Windows.Forms.Button();
+            this.buttonOuvrirPath = new System.Windows.Forms.Button();
+            this.comboBoxWorkingDirectory = new System.Windows.Forms.ComboBox();
+            this.buttonPathSource = new System.Windows.Forms.Button();
+            this.labelPath = new System.Windows.Forms.Label();
+            this.labelStr2Replace = new System.Windows.Forms.Label();
+            this.labelNouveauPrefixPath = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // comboBoxTxt2Change
+            // 
+            this.comboBoxTxt2Change.FormattingEnabled = true;
+            this.comboBoxTxt2Change.Location = new System.Drawing.Point(12, 113);
+            this.comboBoxTxt2Change.Name = "comboBoxTxt2Change";
+            this.comboBoxTxt2Change.Size = new System.Drawing.Size(332, 21);
+            this.comboBoxTxt2Change.TabIndex = 0;
+            this.comboBoxTxt2Change.KeyDown += new System.Windows.Forms.KeyEventHandler(this.comboBoxTxt2Change_KeyDown);
+            // 
+            // comboBoxNouveauPrefix
+            // 
+            this.comboBoxNouveauPrefix.FormattingEnabled = true;
+            this.comboBoxNouveauPrefix.Location = new System.Drawing.Point(12, 165);
+            this.comboBoxNouveauPrefix.Name = "comboBoxNouveauPrefix";
+            this.comboBoxNouveauPrefix.Size = new System.Drawing.Size(332, 21);
+            this.comboBoxNouveauPrefix.TabIndex = 1;
+            this.comboBoxNouveauPrefix.KeyDown += new System.Windows.Forms.KeyEventHandler(this.comboBoxNouveauPrefix_KeyDown);
+            // 
+            // buttonExecuter
+            // 
+            this.buttonExecuter.Location = new System.Drawing.Point(15, 204);
+            this.buttonExecuter.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.buttonExecuter.Name = "buttonExecuter";
+            this.buttonExecuter.Size = new System.Drawing.Size(86, 23);
+            this.buttonExecuter.TabIndex = 2;
+            this.buttonExecuter.Text = "Executer";
+            this.buttonExecuter.UseVisualStyleBackColor = true;
+            this.buttonExecuter.Click += new System.EventHandler(this.buttonExecuter_Click);
+            // 
+            // buttonOuvrirPath
+            // 
+            this.buttonOuvrirPath.Location = new System.Drawing.Point(127, 202);
+            this.buttonOuvrirPath.Name = "buttonOuvrirPath";
+            this.buttonOuvrirPath.Size = new System.Drawing.Size(122, 25);
+            this.buttonOuvrirPath.TabIndex = 6;
+            this.buttonOuvrirPath.Text = "OuvrirPathDestination";
+            this.buttonOuvrirPath.UseVisualStyleBackColor = true;
+            // 
+            // comboBoxWorkingDirectory
+            // 
+            this.comboBoxWorkingDirectory.FormattingEnabled = true;
+            this.comboBoxWorkingDirectory.Location = new System.Drawing.Point(12, 67);
+            this.comboBoxWorkingDirectory.Name = "comboBoxWorkingDirectory";
+            this.comboBoxWorkingDirectory.Size = new System.Drawing.Size(332, 21);
+            this.comboBoxWorkingDirectory.TabIndex = 10;
+            this.comboBoxWorkingDirectory.KeyDown += new System.Windows.Forms.KeyEventHandler(this.comboBoxWorkingDirectory_KeyDown);
+            // 
+            // buttonPathSource
+            // 
+            this.buttonPathSource.Location = new System.Drawing.Point(350, 65);
+            this.buttonPathSource.Name = "buttonPathSource";
+            this.buttonPathSource.Size = new System.Drawing.Size(30, 23);
+            this.buttonPathSource.TabIndex = 11;
+            this.buttonPathSource.Tag = resources.GetString("buttonPathSource.Tag");
+            this.buttonPathSource.Text = "...";
+            this.buttonPathSource.UseVisualStyleBackColor = true;
+            this.buttonPathSource.Click += new System.EventHandler(this.buttonPathSource_Click);
+            // 
+            // labelPath
+            // 
+            this.labelPath.AutoSize = true;
+            this.labelPath.Location = new System.Drawing.Point(9, 43);
+            this.labelPath.Name = "labelPath";
+            this.labelPath.Size = new System.Drawing.Size(54, 13);
+            this.labelPath.TabIndex = 12;
+            this.labelPath.Text = "PathDeW";
+            // 
+            // labelStr2Replace
+            // 
+            this.labelStr2Replace.AutoSize = true;
+            this.labelStr2Replace.Location = new System.Drawing.Point(12, 97);
+            this.labelStr2Replace.Name = "labelStr2Replace";
+            this.labelStr2Replace.Size = new System.Drawing.Size(93, 13);
+            this.labelStr2Replace.TabIndex = 13;
+            this.labelStr2Replace.Text = "ChaineAremplacer";
+            // 
+            // labelNouveauPrefixPath
+            // 
+            this.labelNouveauPrefixPath.AutoSize = true;
+            this.labelNouveauPrefixPath.Location = new System.Drawing.Point(12, 149);
+            this.labelNouveauPrefixPath.Name = "labelNouveauPrefixPath";
+            this.labelNouveauPrefixPath.Size = new System.Drawing.Size(110, 13);
+            this.labelNouveauPrefixPath.TabIndex = 14;
+            this.labelNouveauPrefixPath.Text = "ChaineNouveauPrefix";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(173, 13);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "[enter] pour valider nouvelle chaine";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(392, 255);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labelNouveauPrefixPath);
+            this.Controls.Add(this.labelStr2Replace);
+            this.Controls.Add(this.labelPath);
+            this.Controls.Add(this.buttonPathSource);
+            this.Controls.Add(this.comboBoxWorkingDirectory);
+            this.Controls.Add(this.buttonOuvrirPath);
+            this.Controls.Add(this.buttonExecuter);
+            this.Controls.Add(this.comboBoxNouveauPrefix);
+            this.Controls.Add(this.comboBoxTxt2Change);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox comboBoxTxt2Change;
+        private System.Windows.Forms.ComboBox comboBoxNouveauPrefix;
+        private System.Windows.Forms.Button buttonExecuter;
+        private System.Windows.Forms.Button buttonOuvrirPath;
+        private System.Windows.Forms.ComboBox comboBoxWorkingDirectory;
+        private System.Windows.Forms.Button buttonPathSource;
+        private System.Windows.Forms.Label labelPath;
+        private System.Windows.Forms.Label labelStr2Replace;
+        private System.Windows.Forms.Label labelNouveauPrefixPath;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- lnkTargetChanger/Form1.cs
+++ lnkTargetChanger/Form1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
@@ -7,14 +8,325 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Serialization;
+using System.IO;
 
 namespace lnkTargetChanger
 {
     public partial class Form1 : Form
     {
+        // Les variables globals au formulaire
+        string appDataArterris = "";//c'est dans ce repertoire qu'on a les droits et qu'il convient d'écrire
+        string appdata = "";//son ss rep.
+        bool semaphore = false;
+        static configObject co = new configObject();
+
         public Form1()
         {
             InitializeComponent();
+            appdata = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            appDataArterris = Path.Combine(appdata, "Arterris");
+            XmlSerializer xs = new XmlSerializer(typeof(configObject));//pour serialiser en XML la config (sauvegarde des paths src et dst)
+            co.ListRepertoire2Travail = new List<comboItem>();
+            co.ListPatternARechercher = new List<comboItem>();
+            co.listNouveauPrefix = new List<comboItem>();
+
+            co.strRepertoire2Travail = "";
+            co.strPatternARechercher = "";
+            co.strNouveauPrefix = "";
+
+            if (!Directory.Exists(appDataArterris))
+                Directory.CreateDirectory(appDataArterris);
+
+            if (!File.Exists(appDataArterris + "\\configLNK.xml"))//si le fichier n'existe pas on le cré avec init à "";
+            {
+                co.ListRepertoire2Travail.Add(new comboItem("1", @"c:\temp\lnk"));
+                co.strRepertoire2Travail = "c:\\temp\\lnk";
+                comboBoxWorkingDirectory.Text = co.strRepertoire2Travail;
+
+
+                co.ListPatternARechercher.Add(new comboItem("1", @""));
+                co.strPatternARechercher = "";
+                comboBoxTxt2Change.Text = co.strPatternARechercher;
+
+                co.listNouveauPrefix.Add(new comboItem("1", @"k:\"));
+                co.strNouveauPrefix = @"k:\";
+                comboBoxNouveauPrefix.Text = co.strNouveauPrefix;
+
+                if (!File.Exists(appDataArterris + "\\configLNK.xml"))//si le fichier n'existe pas on le cré avec init à "";
+                {
+
+                    using (StreamWriter wr = new StreamWriter(appDataArterris + "\\configLNK.xml"))
+                    {
+                        xs.Serialize(wr, co);
+                    }
+
+                }
+
+            }
+          
+            using (StreamReader rd = new StreamReader(appDataArterris + "\\configLNK.xml"))
+            {
+                co = xs.Deserialize(rd) as configObject;
+
+               
+            }
+
+            remplirCombo();
         }
+
+        public void remplirCombo()
+        {
+            
+            co.ListRepertoire2Travail.ForEach(i => comboBoxWorkingDirectory.Items.Add(i));
+            co.ListPatternARechercher.ForEach(i => comboBoxTxt2Change.Items.Add(i));
+            co.listNouveauPrefix.ForEach(i => comboBoxNouveauPrefix.Items.Add(i));
+
+            comboBoxWorkingDirectory.Text = co.strRepertoire2Travail;
+            comboBoxTxt2Change.Text = co.strPatternARechercher;
+            comboBoxNouveauPrefix.Text = co.strNouveauPrefix;
+
+        }
+
+
+       
+
+        private void buttonExecuter_Click(object sender, EventArgs e)
+        {
+
+            if (!Directory.Exists(comboBoxWorkingDirectory.Text))
+            {
+                MessageBox.Show("Ce repertoire de travail n'existe pas : " + comboBoxWorkingDirectory.Text);
+                return;
+            }
+            // recup de la liste des fichier .asc du repertoire de la combobox 
+            string[] tabFiles = Directory.GetFileSystemEntries(comboBoxWorkingDirectory.Text, "*.lnk");
+
+            if (tabFiles.Length < 1)
+            {
+                MessageBox.Show("Aucun fichier asc trouvé dans : " + comboBoxWorkingDirectory.Text);
+                return;
+            }
+
+
+
+            for (int i = 0; i < tabFiles.Length; i++)
+            {
+                traiterFichierEnCours(tabFiles[i]);
+            }
+
+            try
+            {
+                System.Diagnostics.Process.Start("explorer.exe", comboBoxWorkingDirectory.Text);
+            }
+            catch (Exception e2)
+            {
+
+                MessageBox.Show(e2.StackTrace);
+            }
+        }
+
+        private void traiterFichierEnCours(String fichierLNK)
+        {
+
+
+            ChangeLinkTarget(fichierLNK);
+
+
+            
+        }
+
+
+        public void ChangeLinkTarget(string shortcutFullPath)
+        {
+            // ToDo verif combo pas vide si possible avant
+            // ToDo sauver .lnk avant changement
+
+            // Load the shortcut.
+            Shell32.Shell shell = new Shell32.Shell();
+            Shell32.Folder folder = shell.NameSpace(Path.GetDirectoryName(shortcutFullPath));
+            Shell32.FolderItem folderItem = folder.Items().Item(Path.GetFileName(shortcutFullPath));
+            Shell32.ShellLinkObject currentLink = (Shell32.ShellLinkObject)folderItem.GetLink;
+
+            if (currentLink.Path.Contains(comboBoxTxt2Change.Text))
+            {
+                // ici creer repertoire sauvegarde selon etat marqeur ou test existance
+                // sauvegarder le lnk
+
+                //changer le repertoire de travail
+                currentLink.WorkingDirectory = currentLink.WorkingDirectory.Replace(comboBoxTxt2Change.Text, comboBoxNouveauPrefix.Text);
+
+                // Assign the new path here. This value is not read-only.
+                currentLink.Path = currentLink.Path.Replace(comboBoxTxt2Change.Text, comboBoxNouveauPrefix.Text);
+                // Save the link to commit the changes.
+                currentLink.Save();
+            }
+
+            //fermer le liens ? 
+        }
+
+        private void buttonPathSource_Click(object sender, EventArgs e)
+        {
+            
+                FolderBrowserDialog fbd = new FolderBrowserDialog();
+
+                DialogResult result = fbd.ShowDialog();
+
+                if (!string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    comboBoxWorkingDirectory.Text = fbd.SelectedPath.ToString();
+                }
+
+                ajouterWorkinglistCombo();
+            
+        }
+
+        private void ajouterWorkinglistCombo()
+        {
+            if (!Directory.Exists(comboBoxWorkingDirectory.Text))
+            {
+                MessageBox.Show("Ce repertoire est introuvable : " + comboBoxWorkingDirectory.Text);
+                return;
+            }
+
+            bool b = co.ListRepertoire2Travail.Any(tr => tr.myValue.Equals(comboBoxWorkingDirectory.Text, StringComparison.CurrentCultureIgnoreCase));
+            if (!b)
+            {
+                //KeyValuePair<string, string> kvp = new KeyValuePair<string, string>(((DicdepotDirectory.Count) + 1).ToString(), comboBoxDepot.Text);
+                comboItem ci = new comboItem(((co.ListRepertoire2Travail.Count) + 1).ToString(), comboBoxWorkingDirectory.Text);
+                co.ListRepertoire2Travail.Add(ci);
+                comboBoxWorkingDirectory.Items.Add(ci);
+                comboBoxWorkingDirectory.SelectedIndex = comboBoxWorkingDirectory.FindStringExact(ci.myValue);
+            }
+        }
+
+        private void comboBoxWorkingDirectory_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                ajouterWorkinglistCombo();
+            }
+        }
+
+        private void ajouterPatternArechercherListCombo()
+        {
+           
+            bool b = co.ListPatternARechercher.Any(tr => tr.myValue.Equals(comboBoxTxt2Change.Text, StringComparison.CurrentCultureIgnoreCase));
+            if (!b)
+            {
+                //KeyValuePair<string, string> kvp = new KeyValuePair<string, string>(((DicdepotDirectory.Count) + 1).ToString(), comboBoxDepot.Text);
+                comboItem ci = new comboItem(((co.ListPatternARechercher.Count) + 1).ToString(), comboBoxTxt2Change.Text);
+                co.ListPatternARechercher.Add(ci);
+                comboBoxTxt2Change.Items.Add(ci);
+                comboBoxTxt2Change.SelectedIndex = comboBoxTxt2Change.FindStringExact(ci.myValue);
+            }
+        }
+
+        private void comboBoxTxt2Change_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                ajouterPatternArechercherListCombo();
+            }
+        }
+
+        private void ajouterNouveauPrefixListCombo()
+        {
+
+            bool b = co.listNouveauPrefix.Any(tr => tr.myValue.Equals(comboBoxNouveauPrefix.Text, StringComparison.CurrentCultureIgnoreCase));
+            if (!b)
+            {
+                //KeyValuePair<string, string> kvp = new KeyValuePair<string, string>(((DicdepotDirectory.Count) + 1).ToString(), comboBoxDepot.Text);
+                comboItem ci = new comboItem(((co.listNouveauPrefix.Count) + 1).ToString(), comboBoxNouveauPrefix.Text);
+                co.listNouveauPrefix.Add(ci);
+                comboBoxNouveauPrefix.Items.Add(ci);
+                comboBoxNouveauPrefix.SelectedIndex = comboBoxNouveauPrefix.FindStringExact(ci.myValue);
+            }
+        }
+
+        private void comboBoxNouveauPrefix_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                ajouterNouveauPrefixListCombo();
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            creatXML();
+        }
+
+        public void creatXML()
+        {
+
+            if (co == null)//un min de verif qd mm
+                return;
+
+            try
+            {
+
+                XmlSerializer xs = new XmlSerializer(typeof(configObject));
+                using (StreamWriter wr = new StreamWriter(appDataArterris + "\\configLNK.xml"))
+                {
+                    xs.Serialize(wr, co);
+                }
+
+                //MessageBox.Show("Enregitrement des paramétres bien effectué \n\r" + "Source: " + co.strSourcePath + "\n\r" + "Destination: " + co.strDestinationPath);
+
+            }
+            catch (Exception e)
+            {
+
+                MessageBox.Show("Erreur lors de la sauvegarde des paramètres: " + e.StackTrace.ToString());
+            }
+
+        }
+    }
+
+
+
+
+    public class configObject
+    {
+
+        public configObject()
+        {
+
+        }
+
+        public String strRepertoire2Travail;
+        public String strPatternARechercher;
+        public String strNouveauPrefix;
+        public List<comboItem> ListRepertoire2Travail;
+        public List<comboItem> ListPatternARechercher;
+        public List<comboItem> listNouveauPrefix;
+    }
+
+    [Serializable]
+    public class comboItem
+    {
+        public comboItem()
+        {
+
+        }
+
+        public comboItem(string k, string v)
+        {
+            myKey = k;
+            myValue = v;
+        }
+
+        [XmlElement("StringElementKey")]
+        public String myKey { get; set; }
+
+        [XmlElement("StringElementValue")]
+        public String myValue { get; set; }
+
+        public override string ToString()
+        {
+            return myValue;
+        }
+
     }
 }
